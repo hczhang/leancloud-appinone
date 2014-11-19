15610d2f42e86c085fa59a8b990b7a56becae579
hczhang <huacheng.zhang@gmail.com>
2014-11-19 09:13:13 +0800

    changed to POST

--- cloud/main.js
+++ cloud/main.js
@@ -2,19 +2,22 @@ require("cloud/app.js");
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	response.success("Hello world!");
 });
 
 AV.Cloud.beforeSave("Messages", function(request,response){
 	var message = request.object.get("data");
 
 	AV.Cloud.httpRequest({
-	  method: 'GET',
+	  method: 'POST',
 	  url: 'http://183.247.151.178:2090/interface/GetInfoService.aspx',
-	  params: {
-		'module':'SyncMessage',
-		'data':message
+		params: {
+			'module':'SyncMessage'
 	  },
+		headers: {
+    	'Content-Type': 'application/json'
+		},
+		body: message,
 	  success: function(httpResponse) {
 	    console.log(httpResponse.text);
 	  },
